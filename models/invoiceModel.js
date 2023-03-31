ce25fedf34c4f6df173c1fe1d0b2083982c58e63
Base <orockbessonglawrence@gmail.com>
2023-03-31 17:02:36 +0100

    matching schema name

--- models/invoiceModel.js
+++ models/invoiceModel.js
@@ -33,7 +33,7 @@ const invoiceSchema = new Schema({
                 }
             }],
         
-            boneTests:[{
+            ctScanTests:[{
                 id:{
                     type: Number
                 },
